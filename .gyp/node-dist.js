7ad6a82911b1d580a0fd97bad8530ba825e2adff
Keren DONG <keren.dong@kungfu.link>
2021-08-15 12:27:52 +0800

    Include headers

--- .gyp/node-dist.js
+++ .gyp/node-dist.js
@@ -10,8 +10,15 @@ const dist = (buildType) => {
   const include = (p) => fse.lstatSync(p).isFile() && exts.includes(path.extname(p));
   const copy = (p) => fse.copySync(p, path.join(nodeDistDir, path.basename(p)));
   const copyFiles = (pattern) => glob.sync(pattern).filter(include).forEach(copy);
+  const copyHeaders = (source) => {
+    const target = path.join(nodeDistDir, 'include');
+    glob.sync(path.join(source, '**', '*.h')).forEach((p) => {
+      fse.copySync(p, path.join(target, p.replace(source, '')));
+    });
+  };
   const makeSymbolLink = (pattern, ext) => {
-    const link = (p) => fse.symlinkSync(path.basename(p), path.join(nodeDistDir, `libnode.${ext}`));
+    const target = path.join(nodeDistDir, `libnode.${ext}`);
+    const link = (p) => fse.symlinkSync(path.basename(p), target);
     glob.sync(path.join(nodeDistDir, pattern)).sort().reverse().slice(0, 1).forEach(link);
   };
 
@@ -20,6 +27,10 @@ const dist = (buildType) => {
 
   copyFiles(path.join('build', buildType, '*.*'));
   copyFiles(path.join('node', 'out', buildType, 'libnode*'));
+
+  copyHeaders(path.join('node', 'src'));
+  copyHeaders(path.join('node', 'deps', 'v8', 'include'));
+  copyHeaders(path.join('node', 'deps', 'uv', 'include'));
 
   makeSymbolLink('libnode.*.dylib', 'dylib');
   makeSymbolLink('libnode.so.*', 'so');
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const path = require('path');
 
-exports.libpath = path.resolve(path.join(__dirname, 'dist', 'node'));
+const distDir = path.join(__dirname, 'dist', 'node');
+
+exports.include = path.resolve(path.join(distDir, 'include'));
+exports.libpath = path.resolve(distDir);
