61c2214b92affc09de39560f43c79cc68d803b72
Keren DONG <keren.dong@kungfu.link>
2022-06-02 16:35:56 +0800

    Fix npm config

--- .gyp/node-npm-config.js
+++ .gyp/node-npm-config.js
@@ -5,13 +5,14 @@ const path = require('path');
 const { spawnSync } = require('child_process');
 
 const PrebuiltHostConfig = 'binary_host_mirror';
-const PrebuiltHost_CN = 'https://prebuilt.libkungfu.cc';
 const PrebuiltHost_US = 'https://prebuilt.libkungfu.io';
 
 const spawnOptsPipe = { shell: true, stdio: 'pipe', windowsHide: true };
 const spawnOptsInherit = { shell: true, stdio: 'inherit', windowsHide: true };
 
 const packageJson = JSON.parse(fs.readFileSync(path.resolve(path.dirname(__dirname), 'package.json')));
+const key = packageJson.binary.module_name;
+const npmConfigKey = `${key}_${PrebuiltHostConfig}`;
 
 const scope = (npmConfigValue) => (npmConfigValue === 'undefined' ? '[package.json]' : '[user]');
 
@@ -32,29 +33,15 @@ function getNpmConfigValue(key) {
 }
 
 function showAllConfig() {
-  const key = packageJson.binary.module_name;
-  const npmConfigKey = `${key}_${PrebuiltHostConfig}`;
   const hostConfigValue = getNpmConfigValue(npmConfigKey);
   const value = hostConfigValue === 'undefined' && packageJson ? packageJson.binary.host : hostConfigValue;
   console.log(`[binary] ${npmConfigKey} = ${value} ${scope(hostConfigValue)}`);
 }
 
-async function main() {
-  await require('sywac')
-    .command('show', { run: showAllConfig })
-    .command('auto', {
-      run: () => {
-        const githubActions = process.env.CI && process.env.GITHUB_ACTIONS;
-        const prebuiltHost = githubActions ? PrebuiltHost_US : PrebuiltHost_CN;
-        const setConfig = (key, value) => githubActions && npmCall(['config', 'set', key, value]);
-        setConfig(`link_node_${PrebuiltHostConfig}`, prebuiltHost);
-        showAllConfig();
-      },
-    })
-    .help('--help')
-    .version('--version')
-    .showHelpByDefault()
-    .parseAndExit();
+if (require.main === module && process.env.CI && process.env.GITHUB_ACTIONS) {
+  npmCall(['config', 'set', npmConfigKey, PrebuiltHost_US]);
 }
 
-if (require.main === module) main().catch(console.error);
+if (require.main === module) {
+  showAllConfig();
+}
